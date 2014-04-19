dab41adce610df527e2a28779cd4b9567f32e9a5
kingnerdthethird <kingnerdthethird@outlook.com>
2014-04-19 11:14:53 -0600

    Finished Problem 5
    I figured out that you don't need to check all the numbers, just the
    ones that don't have multiples. I created an array to hold the numbers
    to check. I then checked each number against all the numbers in the
    array. If one of the numbers didn't match it was skipped instantly. I
    also went up by 20 each time because the number has to be a multiple of
    20. -Isaac

--- Euler_5/Euler_5/Euler_5.cpp
+++ Euler_5/Euler_5/Euler_5.cpp
@@ -2,10 +2,43 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
-
+int Prime_Factor()
+{
+	int i;
+	long long int num = 20;
+	bool notFound = true; // tells if the number has been found 
+	bool skip; // tells the program if it should skip the number
+	const int checks[10] = {
+		20, 19, 18, 17, 16, 15, 14, 13, 12, 11, // you dont need to check all numbers
+	};
+	while (notFound) //while the number hasn't been found
+	{
+		skip = false;
+		num += 20; // go up by 20 each time because it has to be divisible by 20
+		for (i = 0; i < 10; i++)
+		{
+			if (num % checks[i])
+			{
+				skip = true;
+			}
+		}
+		if (skip)
+		{
+			continue;
+		}
+		cout << "Number is: " << num << endl;
+		notFound = false;
+	}
+	return num;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Prime_Factor();
+	cin.get();
+	cin.get();
 	return 0;
 }
 
