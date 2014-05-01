430bbb1ce102973570974ef6d7cebaa6fe367be1
kingnerdthethird <kingnerdthethird@outlook.com>
2014-05-01 06:30:43 -0600

    Continued work on Euler 7

--- C++/Euler_7/Euler_7/Euler_7.cpp
+++ C++/Euler_7/Euler_7/Euler_7.cpp
@@ -5,50 +5,34 @@
 #include <iostream>
 using namespace std;
 
-bool Check_Prime(int num)
-{
-	int temp;
-	for (int i = 1; i < num; i++)
-	{
-		temp = num % i;
-		if (temp = 0)
-		{
-			return false;
-
-		}
-		else
-		{
-			cout << num << " is prime" << endl;
-			return true;
-		}
-	}
-}
-
 int Fill_Array()
 {
+	int place = 0;
+	int num = 1;
 	int i;
-	int place;
-	place = 0;
-	int prime[10000]{};
-	for (i = 1; i++;)
+	int p;
+	cout << "it works!" << endl;
+	while (place <= 10001)
 	{
-		if (Check_Prime(i))
+		for (i = 1;; i++)
 		{
-			cout << i << " is prime" << endl;
-			prime[place] = i;
+			p = num % i;
+			if (p = 0)
+			{
+			place++;
+			cout << num << endl;
+			}
 		}
-		else
-		{
-			cout << i << " does not work" << endl;
-		}
-		place++;
+		cout << "next" << endl;
+		num++;
 	}
-	cout << prime[10000] << endl;
-	return place;
+
+	return num;
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Fill_Array;
+	cout << "test" << endl;
+	Fill_Array();
 	cin.get();
 	cin.get();
 	return 0;
