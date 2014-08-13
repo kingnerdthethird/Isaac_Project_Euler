872dda77745e86f02718b975ec43c349e3827162
KingNerdTheThird <kingnerdthethird@outlook.com>
2014-08-12 18:16:54 -0600

    Added comments to Problem 1
    I'm going through and adding comments to each problem. Just finished
    with Problem 1.
    -Isaac

--- C++/Euler_1/Euler_1/Euler_1.cpp
+++ C++/Euler_1/Euler_1/Euler_1.cpp
@@ -5,27 +5,27 @@
 #include <iostream>
 using namespace std;
 
-int Total(int total, int n)
+int Total(int total, int n) //finds the total
 {
-	for (int n = 1; n < 1000; ++n)
+	for (int n = 1; n < 1000; ++n) //runs while n is less than 1000
 	{
-		if (n % 3 == 0 || n % 5 == 0)
+		if (n % 3 == 0 || n % 5 == 0) //runs if divisible by 3 or 5
 		{
-			total += n;
-			cout << total << endl;
+			total += n; //adds to total
+			cout << total << endl; //states total
 		}
 		else
 		{
-			cout << "Finding Next Total:" << endl;
+			cout << "Finding Next Total:" << endl; //states that the prgram is moving to next total
 		}
 	}
-	return total;
+	return total; //returns the total
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Total(0, 0);
-	cin.get();
-	cin.get();
+	Total(0, 0); //runs the function
+	cin.get(); //not needed, pauses
+	cin.get(); //not needed, pauses
 	return 0;
 }
 
