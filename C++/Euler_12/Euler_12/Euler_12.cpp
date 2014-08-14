e275cb88d44334811993bc2796e8d051e542c325
KingNerdTheThird <kingnerdthethird@outlook.com>
2014-08-14 12:32:11 -0600

    Finished Adding Comments to Problem 2
    I finally finished adding comments to Problem 2. I had to completely
    rewrite the whole program because I had forgotten what it was supposed
    to do (oops!). Since I'm done with the first ten problems and can't
    rally do the next ones right now I'll probably finish up adding comments
    to the rest of my code and then head on over to do all the problems in
    C#.
    -Isaac

---
+++ C++/Euler_12/Euler_12/Euler_12.cpp
@@ -0,0 +1,12 @@
+// Euler_12.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
