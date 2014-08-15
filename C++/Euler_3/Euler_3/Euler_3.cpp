f5fadafc10c975147ab12ca572853ee855b2938a
KingNerdTheThird <kingnerdthethird@outlook.com>
2014-08-14 18:14:38 -0600

    Added Comments to Problem 3
    I added comments to Problem 3. This should make it easier to understand.
    -Isaac

--- C++/Euler_3/Euler_3/Euler_3.cpp
+++ C++/Euler_3/Euler_3/Euler_3.cpp
@@ -5,42 +5,42 @@
 #include <iostream> 
 using namespace std;
 
-int is_prime()
+int is_prime() //function to do everything
 {
 	long long int n = 600851475143; //number
 	long long int f = 0; //factor
 	long long int p = 0; //prime factor
 	long long int c; //count
 	long long int c2; //count 2
-	int check = 0;
+	int check = 0; //check for true 
 
-	for (c = 2; c < n; c++)
+	for (c = 2; c < n; c++) //runs until c is less than but not qual to n
 	{
-		if (n % c == 0)
+		if (n % c == 0) //runs if the remainder of n divided by c is 0
 		{
-			f = c;
+			f = c; //sets f to c
 		}
-		for (c2 = 2; c2 < f; c2++)    
+		for (c2 = 2; c2 < f; c2++) //runs until c2 is less than but not equal to f    
 		{
-			if (f % c2 == 0)    
+			if (f % c2 == 0) //runs if the remainder of f divided by c2 is 0
 			{
-				check = 1;
+				check = 1; //sets check to true
 			}
 		}
 
-			if (check == 0)    
+			if (check == 0) //runs if check is false
 			{
-				p = f;
+				p = f; //sets p to f
 			}
 	
 	}	
-	cout << p << endl;
-	return p;
+	cout << p << endl; //states the prime factor 
+	return p; //returns the value of p
 }
 int main()
 {
-	is_prime();
-	cin.get();
-	cin.get();
+	is_prime(); //runs is_prime
+	cin.get(); //pauses
+	cin.get(); //pauses
 	return 0;
 }
