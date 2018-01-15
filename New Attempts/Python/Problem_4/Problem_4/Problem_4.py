ea1af028e340e2f155add1a5fd5866790f5edd68
Isaac <kingnerdthethird@outlook.com>
2018-01-14 19:28:18 -0700

    Finished Euler 4 in Python
    I learned that you can reverse a string (for example, s) in python with extended slice notation:
    
    r = s[::-1]
    
    where r is the reversed string. Cool shit

--- New Attempts/Python/Problem_4/Problem_4/Problem_4.py
+++ New Attempts/Python/Problem_4/Problem_4/Problem_4.py
@@ -1,26 +1,9 @@
-def palindrome(number):
-    x = number
-    y = 0
-    pal = False
-    while(x > 0):
-        y *= 10
-        y += x % 10
-        x /= 10
-    if(y == number):
-        pal = True
-    return pal;
+number = 0
 
-#number = 0
-#temp = 0
+for i in range (999, 100, -1):
+    for j in range (999, 100, -1):
+        s = str(i * j)
+        if (s == s[::-1] and (i * j) > number):
+            number = (i * j)
 
-#for i in range (1000, 100, -1):
-#    for j in range (1000, 100, -1):
-#        temp = i * j
-#        if (temp > number):
-#            if(palindrome(temp)):
-#                number = temp
-
-#print(temp)
-
-print(palindrome(9009))
-print(palindrome(9909))
+print(number)
