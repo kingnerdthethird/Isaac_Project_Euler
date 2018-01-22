f5b9a1a996c36e24d4b5948e070eb01a08d8ed95
Isaac <kingnerdthethird@outlook.com>
2018-01-22 16:35:06 -0500

    messed  up again

--- New Attempts/Python/Problem_4/Problem_4/Problem_4.py
+++ New Attempts/Python/Problem_4/Problem_4/Problem_4.py
@@ -1,9 +1,18 @@
+def Palindrome(number):
+    y, temp = 0, number
+    while temp > 0:
+        y += temp % 10
+        temp /= 10
+
+    if y == number:
+        return True
+    return False
+
 number = 0
 
-for i in range (999, 100, -1):
-    for j in range (999, 100, -1):
-        s = str(i * j)
-        if (s == s[::-1] and (i * j) > number):
+for i in range(1000, 100, -1):
+    for j in range(1000, 100, -1):
+        if (i * j) > number and Palindrome(i * j):
             number = (i * j)
 
-print(number)
+print (number)
