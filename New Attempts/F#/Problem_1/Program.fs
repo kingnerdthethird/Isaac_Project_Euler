d197e31a957da762b9bee0a6dfa7a86446353bd0
kingnerdthethird <kingnerdthethird@outlook.com>
2022-06-28 21:52:17 -0400

    backup

--- New Attempts/F#/Problem_1/Program.fs
+++ New Attempts/F#/Problem_1/Program.fs
@@ -1,13 +1,10 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let divisible i = i % 5 = 0 || i % 3 = 0
+
+    let sum = [ 1 .. 999] |> Seq.filter(divisible) |> Seq.sum           
+
+    printf "%d" sum
     0 // return an integer exit code
--- New Attempts/F#/Problem_2/Program.fs
+++ New Attempts/F#/Problem_2/Program.fs
@@ -1,13 +1,15 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let fib (a, b) = a + b
+
+    let mutable (a, b, c) = (1, 1, 0)
+
+    while c < 100 do
+        c <- fib (a, b)
+        a <- b
+        b <- c
+        printfn "%d" c
+        
     0 // return an integer exit code
